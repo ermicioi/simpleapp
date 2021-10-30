00738820678b08a8576a2d5a2cc395cf6d617a56
Andrei Ermicioi <andrei.ermicioi@gmail.com>
2021-10-30 23:54:08 +0200

    - Added CSV export - Added new postman scripts

--- src/main/java/me/ermicioia/simpleapp/border/BookAuthorController.java
+++ src/main/java/me/ermicioia/simpleapp/border/BookAuthorController.java
@@ -3,6 +3,11 @@ package me.ermicioia.simpleapp.border;
 import lombok.RequiredArgsConstructor;
 import me.ermicioia.simpleapp.control.BookAuthorEntity;
 import me.ermicioia.simpleapp.control.BookAuthorRepository;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,14 +18,17 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Nonnull;
 import javax.validation.Valid;
+import java.io.ByteArrayInputStream;
 import java.util.List;
+import java.util.stream.Collectors;
 
+import static me.ermicioia.simpleapp.border.ControllerConstants.CSV_VERSION_1;
 import static me.ermicioia.simpleapp.border.ControllerConstants.JSON_VERSION_1;
 
 @RestController
 @RequestMapping("/authors")
 @RequiredArgsConstructor
-class AuthorController {
+class BookAuthorController {
 
     private final BookAuthorRepository bookAuthorRepository;
     private final BookAuthorTransformer bookAuthorTransformer;
@@ -28,6 +36,27 @@ class AuthorController {
     @GetMapping(produces = JSON_VERSION_1)
     List<BookAuthorDto> getAuthors() {
         return bookAuthorTransformer.fromEntity(bookAuthorRepository.findAll());
+    }
+
+    @GetMapping(produces = CSV_VERSION_1)
+    ResponseEntity<Resource> getAuthorsCsv() {
+        final List<BookAuthorDto> authors = bookAuthorTransformer.fromEntity(bookAuthorRepository.findAll());
+        final String content = authors.stream()
+                .map(author -> String.format("%s, %s", author.getFirstName(), author.getLastName()))
+                .collect(Collectors.joining("\\n"));
+
+        final byte[] contentBytes = content.getBytes();
+
+        final InputStreamResource resource = new InputStreamResource(new ByteArrayInputStream(contentBytes));
+
+        final HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=authors.csv");
+
+        return ResponseEntity.ok()
+                .headers(headers)
+                .contentLength(contentBytes.length)
+                .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                .body(resource);
     }
 
     @PostMapping(consumes = JSON_VERSION_1)
--- src/main/java/me/ermicioia/simpleapp/border/ControllerConstants.java
+++ src/main/java/me/ermicioia/simpleapp/border/ControllerConstants.java
@@ -6,10 +6,13 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 class ControllerConstants {
 
-    public static final String BASE_VERSION = "application/vnd.simpleapp.api";
+    private static final String BASE_VERSION = "application/vnd.simpleapp.api";
 
-    public static final String JSON_VERSION = BASE_VERSION + "+json";
+    private static final String JSON_VERSION = BASE_VERSION + "+json";
+    private static final String CSV_VERSION = BASE_VERSION + "+csv";
 
     public static final String JSON_VERSION_1 = JSON_VERSION + ";version=1";
+
+    public static final String CSV_VERSION_1 = CSV_VERSION + ";version=1";
 
 }
